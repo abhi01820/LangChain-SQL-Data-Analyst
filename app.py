bfec2a743b76ce5368509ff04c70662bd724ffd2
Abhi <johnabhi744@gmail.com>
2026-07-03 23:23:26 +0530

    created llm model and connected with the api-key

---
+++ app.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import sqlite3
+
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_gemini_response(question,prompt):
+    model=genai.GenerativeModel('gemini-pro')
+    response=model.generative_content([prompt,question])
+
+    return response.text
