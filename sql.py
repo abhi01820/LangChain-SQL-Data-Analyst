2cfcc6f967f9e9e3cb888aea6bb62af89248ddba
Abhi <johnabhi744@gmail.com>
2026-07-03 23:07:58 +0530

    checking the  data query by  randomly inserting the data

--- sql.py
+++ sql.py
@@ -10,3 +10,10 @@ SECTION VARCHAR(25),MARKS INT);
 
 """
 cursor.execute(table_info)
+
+
+cursor.execute("""INSERT INTO STUDENT VALUES ('Abhi', 'Computer Science', 'A', 90)""")
+cursor.execute("""INSERT INTO STUDENT VALUES ('Khila', 'Data Science', 'B', 100)""")
+cursor.execute("""INSERT INTO STUDENT VALUES ('Hardik', 'Computer Science', 'A', 86)""")
+cursor.execute("""INSERT INTO STUDENT VALUES ('Aggarwal', 'DEVOPS', 'A', 50)""")
+cursor.execute("""INSERT INTO STUDENT VALUES ('Lilly', 'DEVOPS', 'A', 35)""")
