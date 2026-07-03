3f3e1bfeda149a5e10d9dce562e766d7aec9b2a7
Abhi <johnabhi744@gmail.com>
2026-07-03 22:58:10 +0530

    created teh table header content

--- sql.py
+++ sql.py
@@ -4,3 +4,9 @@ connection=sqlite3.connect("student.db")
 
 cursor=connection.cursor()
 
+table_info="""
+Create table STUDENT(NAME VARCHAR(25),CLASS VARCHAR(25),
+SECTION VARCHAR(25),MARKS INT);
+
+"""
+cursor.execute(table_info)
